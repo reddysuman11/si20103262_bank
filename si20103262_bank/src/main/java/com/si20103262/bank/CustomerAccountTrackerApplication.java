145b202a639436821012116ce65dc8b2fb4dcb81
Advik Reddy <Advik Reddy@192.168.0.6>
2020-04-06 11:01:37 +0530

    added Cloud client changes

--- si20103262_bank/src/main/java/com/si20103262/bank/CustomerAccountTrackerApplication.java
+++ si20103262_bank/src/main/java/com/si20103262/bank/CustomerAccountTrackerApplication.java
@@ -2,6 +2,7 @@ package com.si20103262.bank;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 public class CustomerAccountTrackerApplication {
